839f6b4d207e8574d99648362235a59af054e7ae
Alex Saxena <saxenaalex@gmail.com>
2023-12-04 17:04:17 +0100

    Added base endpoint for creating a new student

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const connectToDb = require("./config/connectToDb");
 
+const NewStudentData = require("./models/studentFormSchema");
+
 // Express App
 const app = express();
 app.use(express.json());
@@ -19,11 +21,21 @@ app.get("/", (req, res) => {
   res.json({ general: "Kenobi" });
 });
 
-app.post("/newStudent", (req, res) => {
+app.post("/newStudent", async (req, res) => {
   // TODO
   // Get newStudent data from req.body
+  const name = req.body.name;
+  const parents = req.body.nrOfParents;
+  const city = req.body.city;
+
   // Create NewStudent Data
+  const newStudent = await NewStudentData.create({
+    name: name,
+    parents: parents,
+    city: city,
+  });
   // Respond with sucess message
+  res.status(200).json({ newStudent: newStudent });
 });
 
 // Server Startup
