28355a7914ac19097b32d5ccfe0a84272ab783d4
Alex Saxena <saxenaalex@gmail.com>
2023-12-04 14:22:58 +0100

    Adds Connection to DB

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -1,5 +1,17 @@
-function connectToDb() {
-  console.log("Hello from connectToDb");
+// Load Env File | Checks if Local or Prod
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
+const mongoose = require("mongoose");
+
+async function connectToDb() {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("DB Connection Established");
+  } catch (err) {
+    console.error("DB Connection Error -> ", err);
+  }
 }
 
 module.exports = connectToDb;
--- envExample.md
+++ envExample.md
@@ -1,3 +1,4 @@
 ## Example of how the Env file could look like.
 
 PORT=3001
+DB_URL = "mongodb+srv://URL-for-mongodb-database"
