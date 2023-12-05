f42a626119f31265bc13c15cd6d9da6b0704507f
Alex Saxena <saxenaalex@gmail.com>
2023-12-05 15:29:12 +0100

    Fixes typo on Studen Create -> parent

--- server.js
+++ server.js
@@ -25,26 +25,28 @@ app.post("/newStudent", async (req, res) => {
   // TODO
   // Get newStudent data from req.body
   const name = req.body.name;
-  const parents = req.body.nrOfParents;
+  const nrOfParents = req.body.nrOfParents;
   const city = req.body.city;
 
   // Create NewStudent Data
   const newStudent = await NewStudentData.create({
     name: name,
-    parents: parents,
+    nrOfParents: nrOfParents,
     city: city,
   });
   // Respond with sucess message
   res.status(200).json({ newStudent: newStudent });
 });
 
-// Server Startup
+// Server Startup - ENV port or Default
 const PORT = process.env.PORT || 3001;
 
+// Server Startup - Terminal Color & italic
 const blueText = "\x1b[34m";
 const italicText = "\x1b[3m";
 const resetStyle = "\x1b[0m";
 
+// Server Startup - Active port
 app.listen(PORT, () => {
   console.log(
     `${blueText} ${italicText}Server is running on port ${PORT}${resetStyle}`
