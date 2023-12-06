18403d973baf86ae0af9578df32cffdc5873cebe
Alex Saxena <saxenaalex@gmail.com>
2023-12-06 16:15:15 +0100

    Adds endpoint to delete student by ID W Try Catch

--- server.js
+++ server.js
@@ -81,6 +81,27 @@ app.put("/showStudents/:id", async (req, res) => {
   res.status(200).json({ updatedStudent: showStudent });
 });
 
+app.delete("/showStudents/:id", async (req, res) => {
+  // Get ID from URL | take id from url -> params.id
+  const studentID = req.params.id;
+
+  try {
+    // Request -> DELETE Selected Student
+    const result = await NewStudentData.deleteOne({ _id: studentID });
+
+    if (result.deletedCount === 1) {
+      // Respond with Http Status
+      res.json({ message: "Student Deleted" }).status(204);
+    } else {
+      // If the Student with the provided ID doesn't exist
+      res.json({ error: "Student not found" }).status(404);
+    }
+  } catch (error) {
+    console.error("Error deleting student:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 // Server Startup - ENV port or Default
 const PORT = process.env.PORT || 3001;
 
