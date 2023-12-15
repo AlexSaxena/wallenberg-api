7552c063a8a4127cd1266891a8cf3dd344b5dd35
Alex Saxena <saxenaalex@gmail.com>
2023-12-15 11:32:51 +0100

    Adds psudo code for register

--- controllers/authControllers/userController.js
+++ controllers/authControllers/userController.js
@@ -1,6 +1,12 @@
 const User = require("../../models/UserSchema");
 
 function register(req, res) {
+  // Get Username & Password from Req Body
+
+  // Create User with given Data
+
+  // Respond with New User
+
   return "register";
 }
 function login(req, res) {
