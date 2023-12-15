18e2e9b7584d5456960aac270df16ea7b4661760
Alex Saxena <saxenaalex@gmail.com>
2023-12-15 16:00:14 +0100

    Extract username/password from request, Query DB for user by username, Compare PW with hashed DB PW

--- controllers/authControllers/userLoginController.js
+++ controllers/authControllers/userLoginController.js
@@ -1,13 +1,22 @@
 const bcrypt = require("bcryptjs");
 const User = require("../../models/UserSchema");
 
-function login(req, res) {
+async function login(req, res) {
   // Get Username & Password from body
+  const { username, password } = req.body;
 
   // Find User with requested Username
+  const user = await User.findOne({ username: username });
+  if (!user) {
+    return res.sendStatus(404);
+  }
 
   // Compare given Password with found User password Hash
 
+  const passwordMatch = bcrypt.compareSync(password, user.password);
+  if (!passwordMatch) {
+    return res.sendStatus(401);
+  }
   // Create JWT Token
 
   // Respond with user
