076472bfc6b0ac9dc51f1ec280823dc0dd0c2de7
Alex Saxena <saxenaalex@gmail.com>
2023-12-15 15:05:19 +0100

    Adds pseudo code & guide for Login functionality

--- controllers/authControllers/userLoginController.js
+++ controllers/authControllers/userLoginController.js
@@ -2,6 +2,15 @@ const bcrypt = require("bcryptjs");
 const User = require("../../models/UserSchema");
 
 function login(req, res) {
+  // Get Username & Password from body
+
+  // Find User with requested Username
+
+  // Compare given Password with found User password Hash
+
+  // Create JWT Token
+
+  // Respond with user
   return "login";
 }
 
