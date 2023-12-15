784d9f6d8c8e468bd6e3f8c5ba896efe279f212b
Alex Saxena <saxenaalex@gmail.com>
2023-12-15 14:45:42 +0100

    Updates registration W. bcrypt, input validation, error handling.

--- controllers/authControllers/userController.js
+++ controllers/authControllers/userController.js
@@ -1,15 +1,33 @@
+const bcrypt = require("bcryptjs");
 const User = require("../../models/UserSchema");
 
 async function register(req, res) {
-  // Get Username & Password from Req Body
-  const { username, password } = req.body;
-  // Create User with given Data
-  await User.create({ username, password });
-  // Respond with New User
-  res.sendStatus(200);
+  try {
+    // Get Username & Password from Req Body
+    const { username, password } = req.body;
 
-  return "register";
+    // Validation for checking username & password Exist
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ error: "Username and password are required." });
+    }
+
+    // Creates Hash for password (encryption)
+    const hashedPassword = bcrypt.hashSync(password, 8);
+
+    // Create User with given Data
+    await User.create({ username, password: hashedPassword });
+    // Respond with New User
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+
+    // Respond with an error message
+    res.status(400).json({ error: "Registration failed. Please try again." });
+  }
 }
+
 function login(req, res) {
   return "login";
 }
