87d030e484a8032f7846317d5a501b0ff51f5267
Alex Saxena <saxenaalex@gmail.com>
2023-12-18 14:51:53 +0100

    adds pseudo code for auth middleware

--- middlewares/requireAuth.js
+++ middlewares/requireAuth.js
@@ -1,6 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 function requireAuth(req, res, next) {
+  // Find and Read Token off cookies
+
+  // Decode Token
+
+  //  Find User using Decoded data
+
+  // Add user to to Req object
+
+  // Continue if ok -> using next()
   console.log("Hit Auth Middleware");
   next();
 }
